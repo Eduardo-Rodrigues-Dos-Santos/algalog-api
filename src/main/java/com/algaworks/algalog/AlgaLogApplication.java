5e0993d0b3f9f5d520daad8d1cca9f0c3677bb93
Eduardo-Rodrigues-Dos-Santos <Eduardo-Rodrigues-Dos-Santos@proton.me>
2023-09-11 18:50:50 -0300

    update models.

--- src/main/java/com/algaworks/algalog/AlgaLogApplication.java
+++ src/main/java/com/algaworks/algalog/AlgaLogApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlgalogApplication {
+public class AlgaLogApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AlgalogApplication.class, args);
+        SpringApplication.run(AlgaLogApplication.class, args);
     }
 }
